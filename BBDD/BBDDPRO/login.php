b15f9e22b00249a139779ac98f36482e770a414b
Daniel Rodríguez <danielrodriguezsuarezbustillo@gmail.com>
2022-10-25 14:09:17 +0200

    nueva BBDD

---
+++ BBDD/BBDDPRO/login.php
@@ -0,0 +1,18 @@
+<?php
+//function conexion () {
+$servername = "localhost";
+$username = "root";
+$passord = "";
+$bd="mibbdd";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$bd; charset=UTF8", $username,$passord);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}catch (PDOException$e){
+    echo "Connection failed: " . $e->getMessage();
+}
+return $conn;                   
+//}
+?>
