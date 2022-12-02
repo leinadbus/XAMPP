e233f768eddc0b8c17fb56921978fcb64ae83e05
Daniel Rodríguez <danielrodriguezsuarezbustillo@gmail.com>
2022-12-02 13:40:51 +0100

    kkk

---
+++ coookies/practicaCookie/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form>
+    <p>Nombre</p>
+    <input type=text name=nombre >
+    <p>Color</p>
+    <input type=radio name=color value ="gris">
+    <input type=radio name=color value ="blanco">
+    <input type=radio name=color value ="azul">
+    <p>Tipografía</p>
+    <input type=radio name=tipografia value ="azul">
+    <input type=radio name=tipografia value ="azul">
+    <input type=radio name=tipografia value ="azul">
+</form>
+
+
+
+<?php
+
+?>
+</body>
+</html>
